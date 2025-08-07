820de3463be8ff66758e1821f348933efbeefd33
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-08-07 17:28:36 -0600

    feat: fetch products from api

--- app/(store)/[categoryId]/page.tsx
+++ app/(store)/[categoryId]/page.tsx
@@ -1,5 +1,23 @@
+import { CategoryWithProductsResponseSchema } from "@/app/src/schemas"
 
-export default function StorePage() {
+type Params = Promise<{ categoryId: string }>
+
+async function getProducts(categoryId: string) {
+  const url = `${process.env.API_URL}/categories/${categoryId}?products=true`
+
+  const res = await fetch(url)
+  const json = await res.json()
+
+  const products = CategoryWithProductsResponseSchema.parse(json)
+  
+  return products
+}
+
+export default async function StorePage({ params }: { params: Params }) {
+  const { categoryId } = await params
+
+  const products = await getProducts(categoryId)
+
   return (
     <div>Storepage</div>
   )
