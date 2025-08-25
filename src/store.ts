8d28cdbe26fd625f212837e7362996aa04fded5a
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-08-25 08:33:41 -0600

    Add shopping cart zustand logic

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,41 @@
 import { create } from "zustand";
+import { devtools } from 'zustand/middleware'
+import { Product, ShoppingCart } from "./schemas";
 
 interface Store {
   total: number
+  contents: ShoppingCart
+  addToCart: (product: Product) => void
 }
 
 
-export const useStore = create<Store>(() => ({
+export const useStore = create<Store>()(devtools((set, get) => ({
   total: 0,
+  contents: [],
+  addToCart: (product) => {
+    const { id: productId, categoryId, ...data } = product
+    let contents: ShoppingCart = []
 
+    const duplicated = get().contents.findIndex(item => item.productId === productId)
 
-}))
+    if (duplicated >= 0) {
+      //Check if there is enough stock
+      if (get().contents[duplicated].quantity >= get().contents[duplicated].inventory) return 
+
+      contents = get().contents.map(item => item.productId === productId ? {
+        ...item,
+        quantity: item.quantity + 1
+      } : item)
+    } else {
+      contents = [...get().contents, {
+        ...data,
+        quantity: 1,
+        productId
+      }]
+    }
+
+    set(() => ({
+      contents
+    }))
+  }
+})))
