e0ed5aeb69f331bb9e4688d1425d46014e1ac51d
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-09-06 10:52:30 -0600

    feat: start calendar implementation

--- app/admin/sales/page.tsx
+++ app/admin/sales/page.tsx
@@ -1,6 +1,12 @@
+import TransactionFilter from "@/components/transactions/TransactionFilter";
+import Heading from "@/components/ui/Heading";
 
 export default function SalesPage() {
   return (
-    <div>SalesPage</div>
+    <>
+      <Heading>Sales</Heading>
+      <p className="text-lg"> This section displays your sales. Use the calendar to filter by date </p>
+      <TransactionFilter />
+    </>
   )
 }
---
+++ components/transactions/TransactionFilter.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { useState } from "react"
+import Calendar from "react-calendar"
+import "react-calendar/dist/Calendar.css"
+
+type ValuePiece = Date | null
+type Value = ValuePiece | [ValuePiece, ValuePiece]
+
+export default function TransactionFilter() {
+
+  const [date, setDate] = useState<Value>(new Date())
+
+
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10">
+      <div>
+        <Calendar
+          value={date}
+          onChange={setDate}
+        />
+      </div>
+      <div>
+        2
+      </div>
+    </div>
+  )
+}
