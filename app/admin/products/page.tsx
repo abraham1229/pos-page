6ca72695beda58cf3f2077006f2b807a1aa46b19
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-09-10 17:00:22 -0600

    feat: start products view

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,6 +1,27 @@
+import ProductsTable from "@/components/products/ProductsTable";
+import Heading from "@/components/ui/Heading";
+import { ProductsResponseSchema } from "@/src/schemas";
 
-export default function ProductsPage() {
+async function getProducts() {
+  const url = `${process.env.API_URL}/products`
+  const req = await fetch(url)
+  const json = await req.json()
+  const data = ProductsResponseSchema.parse(json)
+  return {
+    products: data.products,
+    total: data.total
+  }
+}
+
+export default async function ProductsPage() {
+  const { products, total } = await getProducts()
+
   return (
-    <div>Products Page</div>
+    <>
+      <Heading>Manage Products</Heading>
+      <ProductsTable
+        products={products}
+      />
+    </>
   )
 }
