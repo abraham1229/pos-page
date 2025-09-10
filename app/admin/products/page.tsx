a683962d6421b2a23e1242417152c1ebc83c18e2
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-09-10 17:24:42 -0600

    feat: add product pagination

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,9 +1,12 @@
 import ProductsTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
+import Pagination from "@/components/ui/Pagination";
 import { ProductsResponseSchema } from "@/src/schemas";
+import { isValidPage } from "@/src/utils";
+import { redirect } from "next/navigation";
 
-async function getProducts() {
-  const url = `${process.env.API_URL}/products`
+async function getProducts(take: number, skip: number) {
+  const url = `${process.env.API_URL}/products?take=${take}&skip=${skip}`
   const req = await fetch(url)
   const json = await req.json()
   const data = ProductsResponseSchema.parse(json)
@@ -13,14 +16,31 @@ async function getProducts() {
   }
 }
 
-export default async function ProductsPage() {
-  const { products, total } = await getProducts()
+type SearchParams = Promise<{ page: string }>
+
+export default async function ProductsPage({ searchParams }: { searchParams: SearchParams }) {
+  const { page } = await searchParams
+  if (!isValidPage(+page)) redirect('/admin/products?page=1')
+
+  const productsPerPage = 10
+  const skip = (+page - 1) * productsPerPage
+
+  const { products, total } = await getProducts(productsPerPage, skip)
+
+  const totalPages = Math.ceil(total / productsPerPage)
+  if (+page > totalPages) redirect('/admin/products?page=1')
 
   return (
     <>
       <Heading>Manage Products</Heading>
       <ProductsTable
         products={products}
+      />
+
+      <Pagination
+        page={+page}
+        totalPages={totalPages}
+        baseUrl="/admin/products"
       />
     </>
   )
