7fddc532af78045acaf6975109bd876427335d5e
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-09-25 12:35:10 -0600

    fix: change idiom in submit button

--- components/cart/SubmitOrderForm.tsx
+++ components/cart/SubmitOrderForm.tsx
@@ -41,7 +41,7 @@ export default function SubmitOrderForm() {
       <input
         type="submit"
         className="mt-5 w-full bg-indigo-600 hover:bg-indigo-700 cursor-pointer text-white uppercase font-bold p-3"
-        value="Confirmar compra"
+        value="Place Order"
       />
     </form>
   )
